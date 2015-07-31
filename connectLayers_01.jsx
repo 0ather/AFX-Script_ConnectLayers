b11dff6c5d84585715569475928c35fe9ced22f3
Florian Lecoq <lecoq.florian@gmail.com>
2015-07-31 19:31:18 +0200

    Layer label selection

--- connectLayers_01.jsx
+++ connectLayers_01.jsx
@@ -1,18 +1,21 @@
 ﻿// Variables
 var currentComp,
         time,
-        initOn,
         compWidth,
         compHeight,
-        selectedLayers,
         selectedFromLayer,
         selectedToLayers,
         layersSize = [],
         layersPosition = [],
         nullsPosition = [],
+        myPanel,
         layersLabels = ["None", "Red", "Yellow", "Aqua", "Pink", "Lavender", "Peach", "Sea Foam", "Blue", "Green", "Purple", "Orange", "Brown", "Fuschia", "Cyan", "Sandstone", "Dark Green"],
-        imageLinkTypeFromOne = (new File($.fileName)).path + "/linkTypeFromOne.png",
-        imageLinkTypeAll = (new File($.fileName)).path + "/linkTypeAll.png";
+        GUIimagesPath = "file:///" + (new File($.fileName)).path,
+        GUIimages = {
+            imageLinkTypeFromOne: GUIimagesPath + "/linkTypeFromOne.png",
+            imageLinkTypeAll: GUIimagesPath + "/linkTypeAll.png",
+            imageLabels: [GUIimagesPath + "/label01.png", ]
+        };
 
  // Layers Size, Layers Position, Layers Anchor Position
 function getLayersProperty(i) {
@@ -105,118 +108,161 @@ function lineFromOneToOther(i) {
 function init() {
 app.beginUndoGroup("monApp");
 
-    currentComp = app.project.activeItem,
-    selectedLayers = currentComp.selectedLayers;
+    currentComp = app.project.activeItem;
     
-    alert(selectedLayers);
-    
-    if (currentComp == null ) { initOn = 0; alert("You need a composition"); }
-    else if (selectedLayers == null || selectedLayers.length < 2) { initOn = 0; alert("You have to select at least 2 layers"); }
+    if (currentComp == null ) { alert("You need a composition"); }
+
     else {
-        initOn = 1,
         layersSize.length = 0,
         layersPosition.length = 0,
         nullsPosition.length = 0,
+        selectedFromLayer.length = 0,
+        selectedToLayer.length = 0,
         time = currentComp.time,
         compWidth = currentComp.width,
         compHeight = currentComp.height;
         
-        for (i=0; i<selectedLayers.length; i++) {
+        /*for (i=0; i<selectedLayers.length; i++) {
             getLayersProperty(i);
             createNulls(i);
             createShapes(i);
                 //alert(selectedLayers[i].label);
-        };
+        };*/
     };
+
 app.endUndoGroup();
 };
 
 // --------------------
 // UI
 
-function myScript(thisObj){
-    function myScript_buildUI(thisObj){
-        var myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette", "My window name", undefined, {resizeable:true});
+function clickLinkTypeFromOne() {
+    init();
+    //SELECT LAYERS PANEL
         
-        res = "group{orientation:'column',\
-                    linkTypePanel: Panel{text:'Link type', orientation:'row',\
-                        linkTypeFromOne: IconButton{text:'linkTypeFromOne', image:'file:///"+imageLinkTypeFromOne+"'},\
-                        linkTypeAll: IconButton{text:'linkTypeAll', image:'file:///"+imageLinkTypeAll+"'},\
-                        myIconButton: IconButton{text:'IconButton', image:'file:///C:/AFX_Scripts/ScriptUI%20Panels/AED_Groups/AED_Isolate.png'},\
-                        myIconButton: IconButton{text:'IconButton', image:'file:///C:/AFX_Scripts/ScriptUI%20Panels/AED_Groups/AED_Isolate.png'},\
-                    },\
-                    selectLayersPanel: Panel{text:'', orientation:'column',\
-                        fromGroup: Panel{text:'Connect FROM', orientation:'row',\
-                            fromSelectedButton: Button{text:'Selected Layer'},\
-                            orText: StaticText{text:'OR'},\
-                            fromColorsList: DropDownList{properties:{items:['']}, size:[100, 25]},\
-                            fromFeedback: StaticText{text:'0 selected layers'},\
-                        },\
-                        toGroup: Panel{text:'Connect TO', orientation:'row',\
-                            toSelectedButton: Button{text:'Selected Layer'},\
-                            orText2: StaticText{text:'OR'},\
-                            toColorsList: DropDownList{properties:{items:['']}, size:[100, 25]},\
-                            toFeedback: StaticText{text:'0 selected layers'},\
-                        },\
-                    },\
-                }";
-
-        myPanel.grp = myPanel.add(res);
-        
-        //--------------------
-        //LINK TYPE PANEL
-        
-        //01
-        
-        //02
-        
-        //03
-        
-        //04
-        
-        //05
-        
-        
-        //--------------------
-        //SELECT LAYERS PANEL
-        
-        //CONNECT FROM
-        
-        //Add labels to dropdown fromColorsList
+        //Add labels to dropdown ColorsList
         for (k=0;k<layersLabels.length;k++) {
-            myPanel.grp.selectLayersPanel.fromGroup.fromColorsList.add("item", layersLabels[k]);
+            //FROM
+            myPanel.grp.selectLayersPanel.fromPanel.fromGroup1.fromColorsList.add("item", layersLabels[k]);
+            myPanel.grp.selectLayersPanel.fromPanel.fromGroup1.fromColorsList.items[k+1].image = GUIimagesPath + "/label"+(k+1)+".png";
+            //TO
+            myPanel.grp.selectLayersPanel.toPanel.toGroup1.toColorsList.add("item", layersLabels[k]);
+            myPanel.grp.selectLayersPanel.toPanel.toGroup1.toColorsList.items[k+1].image = GUIimagesPath + "/label"+(k+1)+".png";
         };
     
         //Click fromSelectedButton
-        myPanel.grp.selectLayersPanel.fromGroup.fromSelectedButton.onClick = function() {
+        myPanel.grp.selectLayersPanel.fromPanel.fromGroup1.fromSelectedButton.onClick = function() {
             if ( currentComp.selectedLayers.length==0 || currentComp.selectedLayers.length>1 ) {
                 alert("Select one layer");
             } else {
+                myPanel.grp.selectLayersPanel.fromPanel.fromGroup1.fromColorsList.selection = 0;
                 selectedFromLayer = currentComp.selectedLayers;
-                myPanel.grp.selectLayersPanel.fromGroup.fromFeedback.text = selectedFromLayer.length+" selected layer";
+                myPanel.grp.selectLayersPanel.fromPanel.fromGroup2.fromFeedback.text = selectedFromLayer.length+" selected layer";
             };
         };
-        
-        //CONNECT TO
 
-        //Add labels to dropdown toColorsList
-        for (l=0;l<layersLabels.length;l++) {
-            myPanel.grp.selectLayersPanel.toGroup.toColorsList.add("item", layersLabels[l]);
-        };
-    
         //Click toSelectedButton
-        myPanel.grp.selectLayersPanel.toGroup.toSelectedButton.onClick = function() {
+        myPanel.grp.selectLayersPanel.toPanel.toGroup1.toSelectedButton.onClick = function() {
             if ( currentComp.selectedLayers.length==0 ) {
                 alert("Select one layer");
-            } else if ( currentComp.selectedLayers.length==1 ) {
-                selectedToLayer = currentComp.selectedLayers;
-                myPanel.grp.selectLayersPanel.toGroup.toFeedback.text = selectedToLayer.length+" selected layer";
             } else {
+                myPanel.grp.selectLayersPanel.toPanel.toGroup1.toColorsList.selection = 0;
                 selectedToLayer = currentComp.selectedLayers;
-                myPanel.grp.selectLayersPanel.toGroup.toFeedback.text = selectedToLayer.length+" selected layers";
+                myPanel.grp.selectLayersPanel.toPanel.toGroup2.toFeedback.text = currentComp.selectedLayers.length==1 ? selectedToLayer.length+" selected layer" : selectedToLayer.length+" selected layers";
             };
         };
     
+        //Choose fromColorsList
+        myPanel.grp.selectLayersPanel.fromPanel.fromGroup1.fromColorsList.onChange = function() {
+            //Reset array
+            selectedFromLayer.length = 0;
+
+            //If no labels
+            if ( myPanel.grp.selectLayersPanel.fromPanel.fromGroup1.fromColorsList.selection.index == 0 ) {
+                selectedFromLayer.length = 0;
+                myPanel.grp.selectLayersPanel.fromPanel.fromGroup2.fromFeedback.text = "0 selected layers";
+                myPanel.grp.selectLayersPanel.fromPanel.fromGroup2.fromFeedback.text.characters = 20;
+            } else {
+                //Find layers with this label
+                for (a=1; a<currentComp.layers.length+1; a++) {
+                    if (selectedFromLayer.length > 1) {
+                        alert("There are more than 1 layer with this label");
+                        myPanel.grp.selectLayersPanel.fromPanel.fromGroup2.fromFeedback.text = "0 selected layers";
+                        break;
+                    } else if ( currentComp.layer(a).label+1 == myPanel.grp.selectLayersPanel.fromPanel.fromGroup1.fromColorsList.selection.index ) {
+                        selectedFromLayer.push(currentComp.layer(a));
+                        myPanel.grp.selectLayersPanel.fromPanel.fromGroup2.fromFeedback.text = selectedFromLayer.length+" selected layer";                      
+                    };
+                };
+                if (selectedFromLayer.length == 0) {
+                    alert("There are no layers with this label");
+                    myPanel.grp.selectLayersPanel.fromPanel.fromGroup2.fromFeedback.text = "0 selected layers";
+                };
+            };
+        };
+    
+        //Choose toColorsList
+        myPanel.grp.selectLayersPanel.toPanel.toGroup1.toColorsList.onChange = function() {
+            //Reset array
+            selectedToLayer.length = 0;
+
+            //If no labels
+            if ( myPanel.grp.selectLayersPanel.toPanel.toGroup1.toColorsList.selection.index == 0 ) {
+                selectedToLayer.length = 0;
+                myPanel.grp.selectLayersPanel.toPanel.toGroup2.toFeedback.text = "0 selected layers";
+            } else {
+                //Find layers with this label
+                for (b=1; b<currentComp.layers.length+1; b++) {
+                    if ( currentComp.layer(b).label+1 == myPanel.grp.selectLayersPanel.toPanel.toGroup1.toColorsList.selection.index ) {
+                        selectedToLayer.push(currentComp.layer(b));
+                        myPanel.grp.selectLayersPanel.toPanel.toGroup2.toFeedback.text = selectedToLayer.length==1 ? selectedToLayer.length+" selected layer" : selectedToLayer.length+" selected layers";
+                    };
+                };
+                if (selectedToLayer.length == 0) {
+                    alert("There are no layers with this label");
+                    myPanel.grp.selectLayersPanel.toPanel.toGroup2.toFeedback.text = "0 selected layers";
+                };
+            };
+        };
+};
+
+function myScript(thisObj){
+    function myScript_buildUI(thisObj){
+        myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette", "My window name", undefined, {resizeable:true});
+        
+        res = "group{orientation:'column',\
+                        linkTypePanel: Panel{text:'Link type', orientation:'row',\
+                                linkTypeFromOne: IconButton{text:'linkTypeFromOne', helpTip:'Create lines from one object', image:'"+GUIimages.imageLinkTypeFromOne+"'},\
+                                linkTypeAll: IconButton{text:'linkTypeAll', helpTip:'Create lines from all objects', image:'"+GUIimages.imageLinkTypeAll+"'},\
+                                myIconButton: IconButton{text:'IconButton', image:'file:///C:/AFX_Scripts/ScriptUI%20Panels/AED_Groups/AED_Isolate.png'},\
+                                myIconButton: IconButton{text:'IconButton', image:'file:///C:/AFX_Scripts/ScriptUI%20Panels/AED_Groups/AED_Isolate.png'},\
+                        },\
+                        selectLayersPanel: Panel{text:'', orientation:'column',\
+                                fromPanel: Panel{text:'Connect FROM',\
+                                        fromGroup1: Group{text:'', orientation:'row',\
+                                                fromSelectedButton: Button{text:'Selected Layer'},\
+                                                orText: StaticText{text:'OR'},\
+                                                fromColorsList: DropDownList{properties:{items:['']}, size:[100, 25]},\
+                                        },\
+                                        fromGroup2: Group{text:'', orientation:'row',\
+                                                fromFeedback: StaticText{text:'', characters:12},\
+                                        },\
+                                },\
+                                toPanel: Panel{text:'Connect TO',\
+                                        toGroup1: Group{text:'', orientation:'row',\
+                                                toSelectedButton: Button{text:'Selected Layer'},\
+                                                orText2: StaticText{text:'OR'},\
+                                                toColorsList: DropDownList{properties:{items:['']}, size:[100, 25]},\
+                                        },\
+                                        toGroup2: Group{text:'', orientation:'row', alignment:['center', 'center'], alignChildren:['center', 'center'],\
+                                                toFeedback: StaticText{text:'', characters:12},\
+                                        },\
+                                },\
+                        },\
+                }";
+
+        myPanel.grp = myPanel.add(res);
+
       //--------------------
       //GENERAL SETTINGS
         
@@ -228,23 +274,20 @@ function myScript(thisObj){
     //    myPanel.grp.minimumSize = myPanel.grp.size;
         
     //Make panel resizeable
-        myPanel.layout.resize();
-        myPanel.onResizing = myPanel.onResize = function(){this.layout.resize()};
+        //myPanel.layout.resize();
+        //myPanel.onResizing = myPanel.onResize = function(){this.layout.resize()};
         
     //--------------------
-    //CLICK
+    //CLICK LINK TYPE PANEL
     
+    //01
         myPanel.grp.linkTypePanel.linkTypeFromOne.onClick = function() {
-            init();
-            if (initOn==1) {
-                myPanel.grp.selectLayersPanel.visible=true;
-            };
+            myPanel.grp.selectLayersPanel.visible=true;
+            clickLinkTypeFromOne();
         };
+    //02
         myPanel.grp.linkTypePanel.linkTypeAll.onClick = function() {
-            init();
-            if (initOn==1) {
-                myPanel.grp.selectLayersPanel.visible=true;
-            };
+            myPanel.grp.selectLayersPanel.visible=true;
         };
         
         return myPanel;
